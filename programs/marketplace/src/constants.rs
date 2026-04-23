a9e2482a571ded38b091494123ed25385abb210e
andriyhrb <mcp402.market@outlook.com>
2026-04-23 16:08:46 +0000

    [registry] marketplace/cargo, constants, lib

--- programs/marketplace/src/constants.rs
+++
@@ -1,9 +0,0 @@
-pub const MARKETPLACE_SEED: &[u8] = b"mcp_marketplace";
-pub const TOOL_SEED: &[u8] = b"tool";
-pub const PAYMENT_SEED: &[u8] = b"payment";
-pub const PUBLISHER_SEED: &[u8] = b"publisher";
-
-pub const MAX_NAME_LEN: usize = 64;
-pub const MAX_DESCRIPTION_LEN: usize = 256;
-pub const MAX_URL_LEN: usize = 128;
-pub const BPS_DENOMINATOR: u64 = 10_000;
